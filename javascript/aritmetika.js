733809692c83861bdd6e16e5a3f3cf8341f60dcb
Internsial_node <61084125+InternsialNode@users.noreply.github.com>
2020-09-05 09:50:49 +0700

    Create aritmetika.js
    Contoh aritmetika (penjumlahan, pembagian, pengurangan, perkalian, dan modulus)

---
+++ javascript/aritmetika.js
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>belajar</title>
+	</head>
+		<h1>aritmetika</h1>
+			<body>
+			<div id="penjumlahan"></div>
+			<div id="pengurangan"></div>
+			<div id="perkalian"></div>
+			<div id="pembagian"></div>
+			<div id="modulusj"></div>
+			<script>
+				var bilangan1 = 7;
+				var bilangan2 = 2;
+				var penjumlahan = bilangan1 + bilangan2;
+				var pengurangan = bilangan1 - bilangan2;
+				var perkalian = bilangan1 * bilangan2;
+				var pembagian = bilangan1 / bilangan2;
+				var modulus = bilangan1 % bilangan2;
+				
+			document.getElementById("penjumlahan").innerHTML = penjumlahan;
+			document.getElementById("pengurangan").innerHTML = pengurangan;
+			document.getElementById("perkalian").innerHTML = perkalian;
+			document.getElementById("pembagian").innerHTML = pembagian;
+			document.getElementById("modulusj").innerHTML = modulus;
+			</script>
+     </body>
+     </html>
